20e637c8f1f23a6dc700e459d186348ee81396b3
Jolietree <joliet.jack@outlook.com>
2017-01-29 23:47:04 +0100

    Update square.js

--- square.js
+++ square.js
@@ -1,9 +1,10 @@
 function setup(){
     createCanvas(1000,1000); 
+    background(255);
 }
 
 function draw(){
-    background(255);
-    square(300, 300, mouseX, mouseY);
+    
+    square(300, 300, 30, 30);
 }
 
